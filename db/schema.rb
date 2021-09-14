c5e6995db85d1d135588ed16ef628d8f5072494b
wataridmm <r.watari.watari@gmail.com>
2021-09-14 12:33:51 +0000

    add book model db migrate

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_09_14_082548) do
+ActiveRecord::Schema.define(version: 2021_09_14_122648) do
+
+  create_table "books", force: :cascade do |t|
+    t.string "title"
+    t.text "body"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
