6d74baee2350ba1b661ea42caf9778876b5222ad
wataridmm <r.watari.watari@gmail.com>
2021-09-14 13:00:25 +0000

    add books controller  new create index show destroy

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,9 @@
 Rails.application.routes.draw do
+  get 'books/new'
+  get 'books/create'
+  get 'books/index'
+  get 'books/show'
+  get 'books/destroy'
   devise_for :users
   root to:'homes#top'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
