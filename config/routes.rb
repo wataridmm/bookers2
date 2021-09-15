438a0dfde7f6a6fc13fe92503c5ec7d4ea2bfe75
wataridmm <r.watari.watari@gmail.com>
2021-09-15 06:27:49 +0000

    routes, model settings

--- config/routes.rb
+++ config/routes.rb
@@ -1,16 +1,8 @@
 Rails.application.routes.draw do
-  get 'homes/edit'
-  get 'homes/index'
-  get 'homes/new'
-  get 'homes/show'
-  get 'homes/top'
-  get 'homes/about'
-  get 'users/show'
-  get 'books/new'
-  get 'books/create'
-  get 'books/index'
-  get 'books/show'
-  get 'books/destroy'
+  resources :homes
+  resources :users
+  resources :books
+
   devise_for :users
   root to:'homes#top'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
