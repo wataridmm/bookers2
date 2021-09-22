a6b23e1dfb36c761ea7628edfccd7ccb31230a4c
wataridmm <r.watari.watari@gmail.com>
2021-09-22 09:48:55 +0000

    add top

--- config/routes.rb
+++ config/routes.rb
@@ -1,15 +1,19 @@
 Rails.application.routes.draw do
   devise_for :users
-  
+  get 'homes/about'
+  get 'users/show'
+  get 'books/show'
+
+
   resources :homes
   resources :users
   resources :books
-  
 
-  
-  
+
+
+
   root to:'homes#top'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  
+
 
 end
