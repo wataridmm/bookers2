90e2470e920c7a8ecd780675c9bf6534eb07b70e
wataridmm <r.watari.watari@gmail.com>
2021-09-27 07:58:54 +0000

    add book update messege

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -30,7 +30,7 @@ class BooksController < ApplicationController
   def update
     @book = Book.find(params[:id])
     @book.update(book_params)
-    redirect_to book_path(@book.id)
+    redirect_to book_path(@book.id), notice: "You have updated book successfully."
   end
 
   def destroy
