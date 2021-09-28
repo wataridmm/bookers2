f7c6eba66852a4da7738097a9596e50284c981b5
wataridmm <r.watari.watari@gmail.com>
2021-09-28 14:46:47 +0000

    fix template

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -7,4 +7,9 @@ class ApplicationController < ActionController::Base
     devise_parameter_sanitizer.permit(:sign_up, keys: [:email])
   end
 
+  # def after_sign_in_path_for(resource)
+  #   user_path
+  # end
+
+
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,6 @@
 class UsersController < ApplicationController
   before_action :correct_user, only: [:edit, :update]
-  
+
   def index
     @users = User.all
     @user = User.find(current_user.id)
@@ -27,7 +27,7 @@ class UsersController < ApplicationController
     @user = User.find(params[:id])
     @books = @user.books.all
     @book = Book.new
-   
+
   end
 
 private
@@ -35,9 +35,9 @@ private
   def user_params
     params.require(:user).permit(:name, :profile_image, :introduction)
   end
-  
+
   def correct_user
-    
+
     @user = User.find(params[:id])
     if @user != current_user
       redirect_to user_path(current_user.id)
